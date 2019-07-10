0881ecf0aa3325ed7b5aca83b990cc468cb70b16
Vince Howard <hincevoward@gmail.com>
2019-07-09 22:36:40 -0400

    Added gun classes to their respective files

---
+++ WaterWorld/Gun/Gun_M4A1.c
@@ -0,0 +1,15 @@
+#include "Gun.c"
+
+class Gun_M4A1 : Gun {
+	override string GetName() {
+		return "M4A1_Black";
+	}
+
+	override TStringArray GetAttachments() {
+		return {"M4_MPBttstck_Black", "M4_MPHndGrd_Black", "M4_CarryHandleOptic", "M9A1_Bayonet"};
+	}
+
+	override string GetAmmoName() {
+		return "Mag_STANAG_30Rnd";
+	}
+}
---
+++ WaterWorld/Gun/Gun_SVD.c
@@ -0,0 +1,15 @@
+#include "Gun.c"
+
+class Gun_SVD : Gun {
+	override string GetName() {
+		return "SVD";
+	}
+	
+	override TStringArray GetAttachments() {
+		return {"PSO1Optic"};
+	}
+
+	override string GetAmmoName() {
+		return "Mag_SVD_10Rnd";
+	}
+}
