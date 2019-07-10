58524f244e41b637632131fbe07457d51b86619e
Forest Belton <forest+win@homolo.gy>
2019-07-09 19:27:31 -0700

    Add Aug and M4 classes

---
+++ WaterWorld/Gun/Gun_AugSteyr.c
@@ -0,0 +1,15 @@
+#include "Gun.c"
+
+class Gun_AugSteyr : Gun {
+    override string GetName() {
+        return "GP_AugSteyr";
+    }
+
+    override string GetAmmoName() {
+        return "Mag_STANAG_30Rnd";
+    }
+
+    override TStringArray GetAttachments() {
+        return {};
+    }
+}
